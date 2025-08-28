7fccc50768d4a106e1aec93755087925d1764d6e
Baconzito <129697818+Baconzito@users.noreply.github.com>
2025-08-27 22:31:35 -0300

    fix conection

--- Fuente/Services/Extern/Conection.py
+++ Fuente/Services/Extern/Conection.py
@@ -2,16 +2,17 @@ import os
 from dotenv import load_dotenv
 import pyodbc
 
-# Configuración de la conexión
-load_dotenv()
+# Obtener la ruta absoluta del archivo .env
+dotenv_path = os.path.join(os.path.dirname(__file__), 'configs.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 try:
-    connection = pyodbc.connect(os.getenv('connection_string'))
+    connection_string = os.getenv('CONNECTION_STRING')
+    print(f"CONNECTION_STRING: {connection_string}")  # Para depuración
+    connection = pyodbc.connect(connection_string)
     print("Conexión exitosa a la base de datos")
 except Exception as e:
     print(f"Error al conectar a la base de datos: {e}")
-
-# Cerrar la conexión
 finally:
     if 'connection' in locals():
         connection.close()
