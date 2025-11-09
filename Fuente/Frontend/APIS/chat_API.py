889a5f3ca86683717a49bab25a36ae653e2c31f2
Baconzito <129697818+Baconzito@users.noreply.github.com>
2025-11-09 13:48:49 -0300

    comentarios

--- Fuente/Frontend/APIS/chat_API.py
+++ Fuente/Frontend/APIS/chat_API.py
@@ -16,7 +16,7 @@ def get_menu():
         return jsonify({'error': str(e)}), 500
     
 @chat_bp.route("/create_chat", methods=["POST"])
-def create_chat():
+def create_chat(): # se guarda el chat_id en cookies igual que el token
     try:
         chat_id = chat_BLL.create_chat(request.json) # recibe token jwt
         if chat_id:
